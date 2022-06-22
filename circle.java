6d2dce69855558817eb76f72dc67fe3fe9c05b99
Siddheshven <94789042+Siddheshven@users.noreply.github.com>
2022-06-22 10:01:34 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,19 @@
+
+//it is assignment 2
+package robotics;
+
+import ch.aplu.robotsim.*;
+public class circle {
+    circle(){
+        
+        TurtleRobot t = new TurtleRobot();
+        t.setTurtleSpeed(100);
+        while(true){ 
+        t.forward(5);
+        t.left(5);
+    }}
+
+public static void main(String args[]){
+    circle m = new circle();
+}}
+
